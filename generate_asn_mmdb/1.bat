cb36e2e157183105a94b8ac259a9ebd962e8974f
belowlevel <1121170088@qq.com>
2025-08-16 10:49:12 +0800

    update

--- generate_asn_mmdb/1.bat
+++ generate_asn_mmdb/1.bat
@@ -7,6 +7,7 @@ curl -O https://ispip.clang.cn/cmcc_ipv6.txt
 curl -O https://ispip.clang.cn/crtc_cidr.txt
 curl -O https://ispip.clang.cn/crtc_ipv6.txt
 curl -O https://ispip.clang.cn/cernet_cidr.txt
+cat cernet_cidr.txt
 curl -O https://ispip.clang.cn/cernet_ipv6.txt
 curl -O https://ispip.clang.cn/gwbn_cidr.txt
 curl -O https://ispip.clang.cn/gwbn_ipv6.txt
