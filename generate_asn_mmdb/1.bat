bdac0d861b83b4d3891391b85433025592621d1c
belowlevel <1121170088@qq.com>
2025-08-16 11:12:16 +0800

    update

--- generate_asn_mmdb/1.bat
+++ generate_asn_mmdb/1.bat
@@ -1,13 +1,13 @@
-curl -O https://ispip.clang.cn/chinatelecom_cidr.txt
+curl -O https://ispip.clang.cn/chinatelecom.txt
 curl -O https://ispip.clang.cn/chinatelecom_ipv6.txt
-curl -O https://ispip.clang.cn/unicom_cnc_cidr.txt
+curl -O https://ispip.clang.cn/unicom_cnc.txt
 curl -O https://ispip.clang.cn/unicom_cnc_ipv6.txt
-curl -O https://ispip.clang.cn/cmcc_cidr.txt
+curl -O https://ispip.clang.cn/cmcc.txt
 curl -O https://ispip.clang.cn/cmcc_ipv6.txt
-curl -O https://ispip.clang.cn/crtc_cidr.txt
-curl -O https://ispip.clang.cn/crtc_ipv6.txt
+# curl -O https://ispip.clang.cn/crtc.txt
+# curl -O https://ispip.clang.cn/crtc_ipv6.txt
 curl -O https://ispip.clang.cn/cernet.txt
 # cat cernet_cidr.txt
 curl -O https://ispip.clang.cn/cernet_ipv6.txt
-curl -O https://ispip.clang.cn/gwbn_cidr.txt
+curl -O https://ispip.clang.cn/gwbn.txt
 curl -O https://ispip.clang.cn/gwbn_ipv6.txt
